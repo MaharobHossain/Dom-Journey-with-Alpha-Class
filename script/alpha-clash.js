0a3a7ae37e535c53a4b5968092b8aa72719c4bb3
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-04-06 12:11:26 +0600

    Change Home Screen to Playground by click

---
+++ script/alpha-clash.js
@@ -0,0 +1,12 @@
+
+function play(){
+    // step 1- hide the home screen
+    const homeSection = document.getElementById('home-section');
+    homeSection.classList.add('hidden');
+    //  console.log(homeSection.classList);
+
+    // step 2- show the play ground
+    const playgroundSection = document.getElementById('playground-section');
+    playgroundSection.classList.remove('hidden');
+    //  console.log(playgroundSection.classList);
+}
